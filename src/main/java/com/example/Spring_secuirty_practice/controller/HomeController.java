f987512400dd98a29ca10c088dccdd003ba9ca28
dope66 <prince628@naver.com>
2023-03-30 22:36:24 +0900

    1st Commit

---
+++ src/main/java/com/example/Spring_secuirty_practice/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.Spring_secuirty_practice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showMain(){
+        return "main";
+    }
+
+}
